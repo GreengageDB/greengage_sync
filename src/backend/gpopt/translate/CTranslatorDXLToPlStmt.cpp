78971061defda4e6c5cafdf47add360bf7e6cc21
Jingyu Wang <wjingyu@vmware.com>
2023-01-20 15:56:43 -0800

    Remove inaccurate FIXME message
    Fixme:
    src/backend/gpopt/translate/CTranslatorDXLToPlStmt.cpp:
    // GPDB_12_MERGE_FIXME: this duplicates a check in ExecInitProjectSet
    
    Action:
    None. Function SanityCheckProjectSetTargetList share some code with
    ExecInitProjectSet. But refactoring isn't trivial and will involve changes to
    upstream .c files. This check will be removed after PR #14302 is merged. For
    now, remove the FIXME tag to avoid confusion.
    
    (cherry picked from commit 4c2f700259bb3a7c058f3bbd8a0d118981a433c1)

--- src/backend/gpopt/translate/CTranslatorDXLToPlStmt.cpp
+++ src/backend/gpopt/translate/CTranslatorDXLToPlStmt.cpp
@@ -3343,7 +3343,6 @@ ContainsSetReturningFuncOrOp(const CDXLNode *project_list_dxlnode,
 	return false;
 }
 
-// GPDB_12_MERGE_FIXME: this duplicates a check in ExecInitProjectSet
 static bool
 SanityCheckProjectSetTargetList(List *targetlist)
 {
