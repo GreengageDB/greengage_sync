eb340aa3cc6d41a6c9e366773dcc1174de77bbc5
Maksim Michkov <m.michkov@arenadata.io>
2026-07-01 13:39:17 +0300

    Resolve conflicts in src/include/catalog/toasting.h

--- src/include/catalog/pg_type_encoding.h
+++ src/include/catalog/pg_type_encoding.h
@@ -41,6 +41,8 @@ FOREIGN_KEY(typid REFERENCES pg_type(oid));
  */
 typedef FormData_pg_type_encoding *Form_pg_type_encoding;
 
+DECLARE_TOAST(pg_type_encoding, 6222, 6223);
+
 DECLARE_UNIQUE_INDEX(pg_type_encoding_typid_index, 6207, on pg_type_encoding using btree(typid oid_ops));
 #define TypeEncodingTypidIndexId	6207
 
