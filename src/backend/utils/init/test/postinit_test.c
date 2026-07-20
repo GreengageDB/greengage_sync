7820946356f3efeb51587fcf2963df34347a600c
Georgy Shelkovy <g.shelkovy@arenadata.io>
2026-07-20 13:39:54 +0500

    add

--- src/backend/utils/init/test/postinit_test.c
+++ src/backend/utils/init/test/postinit_test.c
@@ -17,15 +17,26 @@ _errfinish_impl()
 
 static void expect_ereport(int expect_elevel)
 {
-	expect_value(errstart, elevel, expect_elevel);
-	expect_any(errstart, domain);
+	if (__builtin_constant_p(expect_elevel) && expect_elevel >= ERROR)
+	{
+		expect_value(errstart_cold, elevel, expect_elevel);
+		expect_any(errstart_cold, domain);
+	}
+	else
+	{
+		expect_value(errstart, elevel, expect_elevel);
+		expect_any(errstart, domain);
+	}
 	if (expect_elevel < ERROR)
 	{
 		will_return(errstart, false);
 	}
     else
     {
-		will_return_with_sideeffect(errstart, false, &_errfinish_impl, NULL);
+		if (__builtin_constant_p(expect_elevel)) \
+			will_return_with_sideeffect(errstart_cold, false, &_errfinish_impl, NULL);
+		else
+			will_return_with_sideeffect(errstart, false, &_errfinish_impl, NULL);
     }
 }
 
