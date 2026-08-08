greenplum-db/gpdb

Modify 019_replslot_limit.pl test for Greenplum, take 2
Fixup commit d28d587847f0baf267fb1211be8a8028d49b2919

src/test/recovery/t/019_replslot_limit.pl
@@ -75,6 +75,11 @@ is($result, "reserved|t", 'check that slot is working');
 # The standby can reconnect to master
 $node_standby->start;
 
+# Greenplum: perform CHECKPOINT just before recording restart_lsn. We need to
+# do this because Greenplum replication slot's restart_lsn is set to whichever
+# smaller between the confirmed received lsn and the last checkpoint's redo lsn.
+# Refer commit 055c57d for more context.
+$node_master->safe_psql('postgres', "CHECKPOINT;");
 $start_lsn = $node_master->lsn('write');
 $node_master->wait_for_catchup($node_standby, 'replay', $start_lsn);
 
@@ -102,6 +107,11 @@ is($result, "reserved", 'check that safe_wal_size gets close to the current LSN'
 
 # The standby can reconnect to master
 $node_standby->start;
+# Greenplum: perform CHECKPOINT just before recording restart_lsn. We need to
+# do this because Greenplum replication slot's restart_lsn is set to whichever
+# smaller between the confirmed received lsn and the last checkpoint's redo lsn.
+# Refer commit 055c57d for more context.
+$node_master->safe_psql('postgres', "CHECKPOINT;");
 $start_lsn = $node_master->lsn('write');
 $node_master->wait_for_catchup($node_standby, 'replay', $start_lsn);
 $node_standby->stop;
@@ -117,6 +127,11 @@ $result = $node_master->safe_psql('postgres', "ALTER SYSTEM SET wal_keep_size to
 
 # The standby can reconnect to master
 $node_standby->start;
+# Greenplum: perform CHECKPOINT just before recording restart_lsn. We need to
+# do this because Greenplum replication slot's restart_lsn is set to whichever
+# smaller between the confirmed received lsn and the last checkpoint's redo lsn.
+# Refer commit 055c57d for more context.
+$node_master->safe_psql('postgres', "CHECKPOINT;");
 $start_lsn = $node_master->lsn('write');
 $node_master->wait_for_catchup($node_standby, 'replay', $start_lsn);
 $node_standby->stop;

GreengageDB/greengage_sync

(cherry picked from commit 5e6abead131b80e17313757852e76d2a545451b0)
Adaptation note: same as the commit it fixes up -- the CHECKPOINTs are
issued on $node_primary rather than $node_master, because next's copy of
the test already went through upstream 9d09efb2661 ("tap tests: replace
'master' with 'primary'").  Content is otherwise unchanged, and the
three insertion points sit at the same places as in Greengage 7.
Ported out of queue order (7.x position 337) together with the commit it
fixes up.  Verified in a container: with both applied
019_replslot_limit.pl passes; without them it fails three subtests, each
reporting a wal_status one step further along than expected.

src/test/recovery/t/019_replslot_limit.pl
@@ -86,6 +86,11 @@ is($result, "reserved|t", 'check that slot is working');
 # The standby can reconnect to primary
 $node_standby->start;
 
+# Greenplum: perform CHECKPOINT just before recording restart_lsn. We need to
+# do this because Greenplum replication slot's restart_lsn is set to whichever
+# smaller between the confirmed received lsn and the last checkpoint's redo lsn.
+# Refer commit 055c57d for more context.
+$node_primary->safe_psql('postgres', "CHECKPOINT;");
 $start_lsn = $node_primary->lsn('write');
 $node_primary->wait_for_catchup($node_standby, 'replay', $start_lsn);
 
@@ -117,6 +122,11 @@ is($result, "reserved",
 
 # The standby can reconnect to primary
 $node_standby->start;
+# Greenplum: perform CHECKPOINT just before recording restart_lsn. We need to
+# do this because Greenplum replication slot's restart_lsn is set to whichever
+# smaller between the confirmed received lsn and the last checkpoint's redo lsn.
+# Refer commit 055c57d for more context.
+$node_primary->safe_psql('postgres', "CHECKPOINT;");
 $start_lsn = $node_primary->lsn('write');
 $node_primary->wait_for_catchup($node_standby, 'replay', $start_lsn);
 $node_standby->stop;
@@ -137,6 +147,11 @@ $result = $node_primary->safe_psql('postgres',
 
 # The standby can reconnect to primary
 $node_standby->start;
+# Greenplum: perform CHECKPOINT just before recording restart_lsn. We need to
+# do this because Greenplum replication slot's restart_lsn is set to whichever
+# smaller between the confirmed received lsn and the last checkpoint's redo lsn.
+# Refer commit 055c57d for more context.
+$node_primary->safe_psql('postgres', "CHECKPOINT;");
 $start_lsn = $node_primary->lsn('write');
 $node_primary->wait_for_catchup($node_standby, 'replay', $start_lsn);
 $node_standby->stop;