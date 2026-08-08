greenplum-db/gpdb-archive

Fix inFunction for auto_stats in CTAS, COPY and AlterTable.
Greenplum introduces auto_stats for a long time, please refer to
https://groups.google.com/a/greenplum.org/g/gpdb-dev/c/bAyw2KBP6yE/m/hmoWikrPAgAJ
for details and decision of auto_stats.

auto_stats() now is invoked at the following 7 places:
  1. ProcessQuery()
  2. _SPI_pquery()
  3. postquel_end()
  4. ATExecExpandTableCTAS()
  5. ATExecSetDistributedBy()
  6. DoCopy()
  7. ExecCreateTableAs()

Previously, Place 2, 3 is hard-coded as inside function,
Place 1, 4~7 is hard-coded as not-inside function.
Place 4~7 does not cover the case that COPY or CTAS
is called inside procedure language.

Since in future auto_stats will be removed, for now let's
just to do some simple fix instead of big refactor.

To correctly pass the inFunction parameter for auto_stats()
at Place 4~7 we introduce executor_run_nesting_level to mark
if the program is already under ExecutorRun(). Place 4~7 is
directly taken as Utility and will not call ExecutorRun() if
they are not inside procedure language. This skill is like
the extension `auto_explain`.

For Place 1~3, the context is clear we do not need to check
executor_run_nesting_level.

NOTE: now, Greenplum support create procedure, and invoke
it as call proc() instead of select proc(). To handle this case,
we also have a static module variable to mark the nest level
of standard_ProcessUtility, if the nest level >= 2, means it is
inside a procedure call.

src/backend/tcop/utility.c
@@ -85,7 +85,18 @@
 /* Hook for plugins to get control in ProcessUtility() */
 ProcessUtility_hook_type ProcessUtility_hook = NULL;
 
-/* local function declarations */
+/*
+ * Greenplumn specific code:
+ *   for detailed comments, please refer to the comments at the
+ *   definition of executor_run_nesting_level in execMain.c.
+ *   Greenplum now support create procedure, so auto_stats also
+ *   need to take inside a procedure as inside function call.
+ *   process_utility_nesting_level >= 2 implies in function call
+ *   when calling from procedure.
+ */
+static int process_utility_nesting_level = 0;
+
+/* Local function declarations */
 static void ProcessUtilitySlow(ParseState *pstate,
 							   PlannedStmt *pstmt,
 							   const char *queryString,
@@ -371,18 +382,33 @@ ProcessUtility(PlannedStmt *pstmt,
 	Assert(queryString != NULL);	/* required as of 8.4 */
 
 	/*
-	 * We provide a function hook variable that lets loadable plugins get
-	 * control when ProcessUtility is called.  Such a plugin would normally
-	 * call standard_ProcessUtility().
+	 * Greenplum specific code:
+	 *   Please refer to the comments at the definition of process_utility_nesting_level.
 	 */
-	if (ProcessUtility_hook)
-		(*ProcessUtility_hook) (pstmt, queryString,
-								context, params, queryEnv,
-								dest, completionTag);
-	else
-		standard_ProcessUtility(pstmt, queryString,
-								context, params, queryEnv,
-								dest, completionTag);
+	process_utility_nesting_level++;
+	PG_TRY();
+	{
+		/*
+		 * We provide a function hook variable that lets loadable plugins get
+		 * control when ProcessUtility is called.  Such a plugin would normally
+		 * call standard_ProcessUtility().
+		 */
+		if (ProcessUtility_hook)
+			(*ProcessUtility_hook) (pstmt, queryString,
+									context, params, queryEnv,
+									dest, completionTag);
+		else
+			standard_ProcessUtility(pstmt, queryString,
+									context, params, queryEnv,
+									dest, completionTag);
+		process_utility_nesting_level--;
+	}
+	PG_CATCH();
+	{
+		process_utility_nesting_level--;
+		PG_RE_THROW();
+	}
+	PG_END_TRY();
 }
 
 /*
@@ -4025,4 +4051,10 @@ GetCommandLogLevel(Node *parsetree)
 	}
 
 	return lev;
+}
+
+bool
+utility_nested(void)
+{
+	return process_utility_nesting_level >= 2;
 }

GreengageDB/greengage_sync

(cherry picked from commit ae7e5690336ca3bc6dad6ed15dcacc78545f5be3)
Adaptation note: three conflicts, all pure PostgreSQL 14 divergence.
ProcessUtility()'s last parameter is a QueryCompletion *qc here, not
the PostgreSQL 12 char *completionTag, so the nesting counter wraps the
qc form of the hook dispatch.  The include the commit adds to copy.c
("tcop/utility.h") goes into the much shorter include list that
copy.c has since upstream c532d15dddff split the file, and the
neighbouring includes the original hunk carried as context (storage/fd.h,
storage/execute_pipe.h, tcop/tcopprot.h) live in copyfrom.c/copyto.c
now and were not added here.  In executor.h the declaration of
already_under_executor_run() is appended next to
change_varattnos_of_a_varno(), whose signature already takes an AttrMap *
instead of an AttrNumber *.  No behaviour change.
The test's expected output also needed the PostgreSQL 14 spelling of
"this relation has never been analyzed": upstream 3d351d916b2
("Redefine pg_class.reltuples to be -1 before the first VACUUM or
ANALYZE.") makes pg_class.reltuples -1 instead of 0 there, so the
fourteen INFO lines of test_auto_stats_in_function that assert
auto_stats did NOT run now read ntuples=-1.  The lines where analyze
really did run are unchanged.

src/backend/tcop/utility.c
@@ -86,6 +86,17 @@
 /* Hook for plugins to get control in ProcessUtility() */
 ProcessUtility_hook_type ProcessUtility_hook = NULL;
 
+/*
+ * Greenplumn specific code:
+ *   for detailed comments, please refer to the comments at the
+ *   definition of executor_run_nesting_level in execMain.c.
+ *   Greenplum now support create procedure, so auto_stats also
+ *   need to take inside a procedure as inside function call.
+ *   process_utility_nesting_level >= 2 implies in function call
+ *   when calling from procedure.
+ */
+static int process_utility_nesting_level = 0;
+
 /* local function declarations */
 static int	ClassifyUtilityCommandAsReadOnly(Node *parsetree);
 static void ProcessUtilitySlow(ParseState *pstate,
@@ -539,18 +550,33 @@ ProcessUtility(PlannedStmt *pstmt,
 	Assert(qc == NULL || qc->commandTag == CMDTAG_UNKNOWN);
 
 	/*
-	 * We provide a function hook variable that lets loadable plugins get
-	 * control when ProcessUtility is called.  Such a plugin would normally
-	 * call standard_ProcessUtility().
+	 * Greenplum specific code:
+	 *   Please refer to the comments at the definition of process_utility_nesting_level.
 	 */
-	if (ProcessUtility_hook)
-		(*ProcessUtility_hook) (pstmt, queryString,
-								context, params, queryEnv,
-								dest, qc);
-	else
-		standard_ProcessUtility(pstmt, queryString,
-								context, params, queryEnv,
-								dest, qc);
+	process_utility_nesting_level++;
+	PG_TRY();
+	{
+		/*
+		 * We provide a function hook variable that lets loadable plugins get
+		 * control when ProcessUtility is called.  Such a plugin would normally
+		 * call standard_ProcessUtility().
+		 */
+		if (ProcessUtility_hook)
+			(*ProcessUtility_hook) (pstmt, queryString,
+									context, params, queryEnv,
+									dest, qc);
+		else
+			standard_ProcessUtility(pstmt, queryString,
+									context, params, queryEnv,
+									dest, qc);
+		process_utility_nesting_level--;
+	}
+	PG_CATCH();
+	{
+		process_utility_nesting_level--;
+		PG_RE_THROW();
+	}
+	PG_END_TRY();
 }
 
 /*
@@ -4197,4 +4223,10 @@ GetCommandLogLevel(Node *parsetree)
 	}
 
 	return lev;
+}
+
+bool
+utility_nested(void)
+{
+	return process_utility_nesting_level >= 2;
 }