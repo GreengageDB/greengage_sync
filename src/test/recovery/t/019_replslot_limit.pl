greenplum-db/gpdb

Modify 019_replslot_limit.pl test for Greenplum

src/test/recovery/t/019_replslot_limit.pl
@@ -40,6 +40,11 @@ $node_standby->append_conf('postgresql.conf', "primary_slot_name = 'rep1'");
 
 $node_standby->start;
 
+# Greenplum: perform CHECKPOINT just before recording restart_lsn. We need to
+# do this because Greenplum replication slot's restart_lsn is set to whichever
+# smaller between the confirmed received lsn and the last checkpoint's redo lsn.
+# Refer commit 055c57d for more context.
+$node_master->safe_psql('postgres', "CHECKPOINT;");
 # Wait until standby has replayed enough data
 my $start_lsn = $node_master->lsn('write');
 $node_master->wait_for_catchup($node_standby, 'replay', $start_lsn);

GreengageDB/greengage_sync

(cherry picked from commit d28d587847f0baf267fb1211be8a8028d49b2919)
Adaptation note: next's 019_replslot_limit.pl is the pgindent-wrapped
variant that already went through upstream 9d09efb2661 ("tap tests:
replace 'master' with 'primary'"), so the inserted CHECKPOINT is issued
on $node_primary rather than $node_master.  Content is otherwise
unchanged.
Ported out of queue order (7.x position 306), directly on top of
055c57dd7017 ("Set replication slot's restart_lsn to not greater than
redo location"), which is the commit that makes this test fail: with the
slot's restart_lsn clamped to the last checkpoint's redo lsn, the slot
reads as further behind than upstream expects and wal_status advances a
step early.  Its fixes are ~120 and ~150 queue positions ahead, so
keeping them in order would leave 019_replslot_limit.pl red for several
batches.

src/test/recovery/t/019_replslot_limit.pl
@@ -45,6 +45,11 @@ $node_standby->append_conf('postgresql.conf', "primary_slot_name = 'rep1'");
 
 $node_standby->start;
 
+# Greenplum: perform CHECKPOINT just before recording restart_lsn. We need to
+# do this because Greenplum replication slot's restart_lsn is set to whichever
+# smaller between the confirmed received lsn and the last checkpoint's redo lsn.
+# Refer commit 055c57d for more context.
+$node_primary->safe_psql('postgres', "CHECKPOINT;");
 # Wait until standby has replayed enough data
 my $start_lsn = $node_primary->lsn('write');
 $node_primary->wait_for_catchup($node_standby, 'replay', $start_lsn);