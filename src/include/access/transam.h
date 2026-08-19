3f9016e3435a93771ffb9c23dbb15cbd16b0552a
Evgeniy Ratkov <e.ratkov@arenadata.io>
2026-08-19 15:15:22 +0300

    squash! Fix conflicts in src/include/transam.h (#314)
    Commit ab596105b55f1d7fbd5a66b66f65227d210b047d changed number of OID for
    FirstBootstrapObjectId, but earlier GPBD moved this OIDs to another file
    "pg_magic_oid.h". Update FirstBootstrapObjectId at the pg_magic_oid.h file.

--- src/include/access/transam.h
+++ src/include/access/transam.h
@@ -154,45 +154,6 @@ FullTransactionIdAdvance(FullTransactionId *dest)
 	(AssertMacro(TransactionIdIsNormal(id1) && TransactionIdIsNormal(id2)), \
 	(int32) ((id1) - (id2)) > 0)
 
-<<<<<<< HEAD
-=======
-/* ----------
- *		Object ID (OID) zero is InvalidOid.
- *
- *		OIDs 1-9999 are reserved for manual assignment (see .dat files in
- *		src/include/catalog/).  Of these, 8000-9999 are reserved for
- *		development purposes (such as in-progress patches and forks);
- *		they should not appear in released versions.
- *
- *		OIDs 10000-12999 are reserved for assignment by genbki.pl, for use
- *		when the .dat files in src/include/catalog/ do not specify an OID
- *		for a catalog entry that requires one.
- *
- *		OIDS 13000-16383 are reserved for assignment during initdb
- *		using the OID generator.  (We start the generator at 13000.)
- *
- *		OIDs beginning at 16384 are assigned from the OID generator
- *		during normal multiuser operation.  (We force the generator up to
- *		16384 as soon as we are in normal operation.)
- *
- * The choices of 8000, 10000 and 13000 are completely arbitrary, and can be
- * moved if we run low on OIDs in any category.  Changing the macros below,
- * and updating relevant documentation (see bki.sgml and RELEASE_CHANGES),
- * should be sufficient to do this.  Moving the 16384 boundary between
- * initdb-assigned OIDs and user-defined objects would be substantially
- * more painful, however, since some user-defined OIDs will appear in
- * on-disk data; such a change would probably break pg_upgrade.
- *
- * NOTE: if the OID generator wraps around, we skip over OIDs 0-16383
- * and resume with 16384.  This minimizes the odds of OID conflict, by not
- * reassigning OIDs that might have been assigned during initdb.
- * ----------
- */
-#define FirstGenbkiObjectId		10000
-#define FirstBootstrapObjectId	13000
-#define FirstNormalObjectId		16384
-
->>>>>>> 8ff1c94649f
 /*
  * VariableCache is a data structure in shared memory that is used to track
  * OID and XID assignment state.  For largely historical reasons, there is
