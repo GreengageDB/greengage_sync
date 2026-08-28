a1252ad99cc52b3150a773e24354e26c7426cce5
Denis Kovalev <d.kovalev@arenadata.io>
2026-08-28 11:26:27 +0300

    Quotes and spacing

--- src/backend/utils/cache/relcache.c
+++ src/backend/utils/cache/relcache.c
@@ -4538,9 +4538,8 @@ CheckConstraintFetch(Relation relation)
 		/* protect limited size of array */
 		if (found >= ncheck)
 		{
-			elog(WARNING,
-			     "pg_class reports %d constraint record(s) for relation %s, but found extra in pg_constraint",
-			     ncheck, RelationGetRelationName(relation));
+			elog(WARNING, "pg_class reports %d constraint record(s) for relation \"%s\", but found extra in pg_constraint",
+				ncheck, RelationGetRelationName(relation));
 			break;
 		}
 
