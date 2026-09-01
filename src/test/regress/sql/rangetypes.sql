c731d12baa71a122f48866b79fa10860914738b8
Viktor Kurilko <v.kurilko@arenadata.io>
2026-09-01 12:10:28 +0700

    squash! Fix rangetypes test (#437)
    Commit ac9099fc1dd460bffaafec19272159dd7bc86f5b added a new test to the
    rangetypes.
    Adjust its output to the ggdb. Add an analysis of the test_range_elem table to
    make the output closer to the Postgres upstream.

--- src/test/regress/sql/rangetypes.sql
+++ src/test/regress/sql/rangetypes.sql
@@ -380,6 +380,8 @@ RESET enable_bitmapscan;
 create table test_range_elem(i int4);
 create index test_range_elem_idx on test_range_elem (i);
 insert into test_range_elem select i from generate_series(1,100) i;
+-- GPDB: analyze for stable plan closer to upstream
+analyze test_range_elem;
 
 SET enable_seqscan    = f;
 
